a08b0cba999debf632c59e152d0c8a38cbd0536e
Flyingbiker <schoebel.aymeric@gmail.com>
2021-01-25 16:57:38 +0100

    fin de journée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chronometre';
+  title = 'stopwatch';
   
   public time : number = 0;  
+  public seconds : number = 0;  
+  public minutes : number = 0;  
   public chronoOn : boolean = false;
-  public chrono ;
+  public isPaused : boolean = false;
+  public chrono : number ;
 
   public arrayTime : Array<number> = [];
   
   public startChrono() : number {
-    this.chronoOn = true;
+    this.chronoOn = true; 
+    this.isPaused = false;   
     this.chrono = setInterval(()=>{
       this.time += 1;
+      if (this.time % 100){
+        this.seconds += 1;
+      }
+      if (this.seconds % 60){
+        this.minutes += 1;
+      }
       }, 1000);
     return this.time;
-    // return this.time = Date.now();
   }
 
   public timeElapsed(){
@@ -28,12 +37,11 @@ export class AppComponent {
     return Date.now() - this.time;
   }
 
-  public stopChrono() {
+  public pauseChrono() {    
+    this.isPaused = true;
     clearInterval(this.chrono);
   }
 
-  
-  
 }
 
 
