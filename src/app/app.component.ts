6d814ec75707395785e9ef5efe0ad7c60e8bc84f
Flyingbiker <schoebel.aymeric@gmail.com>
2021-01-25 14:50:07 +0100

    start Chrono method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chronometre';
+  
+  public time : Number;  
+  public arrayTime : Array<Number> = [];
+  
+  public startChrono() : Number {
+    return this.time = Date.now();
+  }
+
+  public timeElapsed(){
+    if ( (typeof this.time) === Number)
+    return Date.now() - this.time;
+  }
 }
+
