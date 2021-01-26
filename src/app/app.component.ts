268cd31a6c39a94eb7e3d9cb4eaa9c9e68739216
Flyingbiker <schoebel.aymeric@gmail.com>
2021-01-26 09:10:30 +0100

    affichage chrono terminé + ajout tableau d'objet pour retenir le temps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,28 +11,26 @@ export class AppComponent {
   public time : number = 0;  
   public seconds : number = 0;  
   public minutes : number = 0;  
+  public hours : number = 0;  
   public chronoOn : boolean = false;
   public isPaused : boolean = false;
   public chrono : number ;
 
-  public arrayTime : Array<number> = [];
+  public arrayTime : Array<object> = [];
   
   public startChrono() : number {
     this.chronoOn = true; 
     this.isPaused = false;   
     this.chrono = setInterval(()=>{
       this.time += 1;
-      if (this.time % 100){
-        this.seconds += 1;
-      }
-      if (this.seconds % 60){
-        this.minutes += 1;
-      }
+      this.seconds = this.time % 60;
+      this.minutes = Math.floor((this.time / 60) % 60);
+      this.hours = Math.floor(this.time / 60 / 60);
       }, 1000);
     return this.time;
   }
 
-  public timeElapsed(){
+  public timeElapsed() : number {
     if ( (typeof this.time) == "number" && this.time != 0)
     return Date.now() - this.time;
   }
@@ -40,6 +38,17 @@ export class AppComponent {
   public pauseChrono() {    
     this.isPaused = true;
     clearInterval(this.chrono);
+  }
+
+  public holdTime() : Array<object> {
+    this.arrayTime.push({
+      "hours": this.hours, 
+      "minutes" : this.minutes,
+      "seconds" : this.seconds
+    })
+    console.log( this.arrayTime );
+    
+    return this.arrayTime;
   }
 
 }
